5fedf4407c2bf137e81d7415b391e043d138984e
bethel-m <bethelcletus87@gmail.com>
2023-01-24 01:15:31 +0100

    added more routes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,status,HTTPException
 import schemas,models
+from typing import List
 from database import engine,SessionLocal
 from sqlalchemy.orm import session
 
@@ -11,13 +12,31 @@ def get_db():
     yield db
   finally:
     db.close
-    
+
 models.Base.metadata.create_all(engine)
 
-@app.post("/blog")
+@app.post("/blog",status_code=status.HTTP_201_CREATED)
 def create_blog(request: schemas.Blog,db: session = Depends(get_db)):
   new_blog = models.Blog(title=request.title,body=request.body)
   db.add(new_blog)
   db.commit()
   db.refresh(new_blog)
   return new_blog
+
+@app.get("/get",status_code=status.HTTP_200_OK,response_model=List[schemas.ShowBlog])
+def all(db:session = Depends(get_db)):
+  blogs = db.query(models.Blog).all()
+  return blogs
+
+@app.get('/blog/{id}',status_code=status.HTTP_200_OK,response_model=schemas.ShowBlog)
+def show(id,db: session = Depends(get_db)):
+  blog = db.query(models.Blog).filter(models.Blog.id==id).first()
+  if not blog:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"blog for id {id} not found")
+  return blog
+
+@app.delete("/blog/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id,db: session = Depends(get_db)):
+  db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
+  db.commit()
+  return {"detail":f"post with id {id} deleted"}
