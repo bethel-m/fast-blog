5bd2cf952fb978893148e1fbc9d2957eed3b5506
bethel-m <bethelcletus87@gmail.com>
2023-01-24 02:03:39 +0100

    created users schema,model and route

--- main.py
+++ main.py
@@ -40,3 +40,11 @@ def destroy(id,db: session = Depends(get_db)):
   db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
   db.commit()
   return {"detail":f"post with id {id} deleted"}
+
+@app.post("/user")
+def create_user(request: schemas.User,db: session = Depends(get_db)):
+  new_user = models.User(name=request.name,email=request.email,password=request.password)
+  db.add(new_user)
+  db.commit()
+  db.refresh(new_user)
+  return new_user
--- schemas.py
+++ schemas.py
@@ -1,10 +1,16 @@
 from pydantic import BaseModel
-
+from typing import Optional
 class Blog(BaseModel):
   title: str 
   body: str
   
 class ShowBlog(BaseModel):
   title: str 
+  body: str
   class Config:
     orm_mode = True
+    
+class User(BaseModel):
+  name: str
+  email: str
+  password: str
